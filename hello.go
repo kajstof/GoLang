6f2a8f033594044c264f188283317f784492eaf2
kajstof <krysiak86@gmail.com>
2017-07-13 21:32:51 +0200

    Adding strings section

--- hello.go
+++ hello.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 import "math"
+import "strings"
+import "sort"
+//import "os"
+//import "log"
+//import "io/ioutil"
+//import "strconv"
 
 func main() {
     fmt.Println("Hello World")
@@ -118,6 +124,20 @@ func main() {
     circ := Circle{4}
     fmt.Println("Rectangle Area =", getArea(rect))
     fmt.Println("Circle Area =", getArea(circ))
+    // String examples
+    sampString := "Hello World"
+	fmt.Println(strings.Contains(sampString, "lo"))
+	fmt.Println(strings.Index(sampString, "lo"))
+	fmt.Println(strings.Count(sampString, "lo"))
+	fmt.Println(strings.Replace(sampString, "l", "x", 3))
+	csvString := "1,2,3,4,5,6"
+	fmt.Println(strings.Split(csvString, ","))
+	listOfLetters := []string{"c", "a", "b"}
+	sort.Strings(listOfLetters)
+	fmt.Println("Letters: ", listOfLetters)
+	listOfNums := strings.Join([]string{"3", "2", "1"}, ", ")
+	fmt.Println("Numbers: ", listOfNums)
+	fmt.Println("Dupa")
 }
 
 func addThemUp(numbers []float64) float64 {
