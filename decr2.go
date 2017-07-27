eea2256a619c182fcdccdd95de5e887567c1a55c
Krzysztof Krysiak <krysiak86@gmail.com>
2017-07-27 22:07:11 +0200

    Update decr2.go

--- decr2.go
+++ decr2.go
@@ -7,7 +7,7 @@ func decr2(n uint64 , c chan bool) {
     c <- true //koniec wykonywania goroutyny
 }
 
-func main2() {
+func main() {
     c := make (chan bool, 4)
     go decr2(10000000000, c) // wszystkie
     go decr2(10000000000, c) // goroutyny piszą
