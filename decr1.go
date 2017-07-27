c41d1d12743e9ff3221d4577bb2d14ead60fe27d
Krzysztof Krysiak <krysiak86@gmail.com>
2017-07-27 21:59:38 +0200

    Update decr1.go

--- decr1.go
+++ decr1.go
@@ -1,14 +1,14 @@
 package main
 
-func decr(n uint64) {
+func decr1(n uint64) {
     for n > 0 {
         n--
     }
 }
 
 func main() {
-    decr(10000000000)
-    decr(10000000000)
-    decr(10000000000)
-    decr(10000000000)
+    decr1(10000000000)
+    decr1(10000000000)
+    decr1(10000000000)
+    decr1(10000000000)
 }
