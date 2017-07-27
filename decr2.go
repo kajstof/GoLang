154aa5d6ba6d48ac6e04d42a9db74b18e8fc214b
Krzysztof Krysiak <krysiak86@gmail.com>
2017-07-27 22:09:31 +0200

    Update decr2.go

--- decr2.go
+++ decr2.go
@@ -1,18 +1,18 @@
-package main
+package decr2
 
-func decr2(n uint64 , c chan bool) {
+func decr(n uint64 , c chan bool) {
     for n > 0 {
         n--
     }
     c <- true //koniec wykonywania goroutyny
 }
 
-func main() {
+func decr2() {
     c := make (chan bool, 4)
-    go decr2(10000000000, c) // wszystkie
-    go decr2(10000000000, c) // goroutyny piszą
-    go decr2(10000000000, c) // do jednego
-    go decr2(10000000000, c) // kanału
+    go decr(10000000000, c) // wszystkie
+    go decr(10000000000, c) // goroutyny piszą
+    go decr(10000000000, c) // do jednego
+    go decr(10000000000, c) // kanału
     <-c //z kanału
     <-c //wyciągnijmy
     <-c //cztery
